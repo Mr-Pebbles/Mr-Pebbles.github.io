1fa8580c9f635014a048191ee2a145c35a7369fc
Mr-Pebbles <benstone272@gmail.com>
2022-02-08 18:32:25 -0500

    fix letter color glitch
    if ever got letter color correct stays green if you use it again in wrong spot

--- main.js
+++ main.js
@@ -394,7 +394,9 @@ function update() {
         } // Is it in the word?
         else if (word.includes(letter)) {
             currTile.classList.add("present");
-			document.getElementById(letter).style.backgroundColor ="GoldenRod";
+			if(!document.getElementById(letter).style.backgroundColor =="green"){
+
+			document.getElementById(letter).style.backgroundColor ="GoldenRod";}
         } // Not in the word
         else {
             currTile.classList.add("absent");
